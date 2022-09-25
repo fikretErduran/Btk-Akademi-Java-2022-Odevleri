a8ab993a342d0115c5c11809e48afa536af9896a
FikretErduran <fikret.erd@gmail.com>
2022-09-25 11:37:53 +0200

    Add files via upload

---
+++ OdevGithub/Ders23_methodOverloading/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem =new DortIslem();
+		System.out.println(dortIslem.topla(2, 7));
+		//dortIslem.topla2(2, 5, 3);
+		System.out.println(dortIslem.topla2(2, 5, 3));
+		
+		
+
+	}
+
+}
