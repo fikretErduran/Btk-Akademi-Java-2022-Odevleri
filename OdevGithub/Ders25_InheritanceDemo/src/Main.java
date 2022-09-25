3efc194d57850258cd1810d3673de1b4d48871ce
FikretErduran <fikret.erd@gmail.com>
2022-09-25 14:01:35 +0200

    Add files via upload

---
+++ OdevGithub/Ders25_InheritanceDemo/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	  //OgretmenKrediManager ogretmenKrediManager=new OgretmenKrediManager();
+	 // ogretmenKrediManager.Hesapla();
+		KrediUI krediUI=new KrediUI();
+		krediUI.KrediHesapla(new AskerKrediManager());
+
+	}
+
+}
