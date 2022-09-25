dac92286b1748c36a5e4fe81c963eb86a315c851
FikretErduran <fikret.erd@gmail.com>
2022-09-25 13:24:52 +0200

    Add files via upload

---
+++ OdevGithub/Ders24_Inheritance/src/EmployeeManager.java
@@ -0,0 +1,16 @@
+
+public class EmployeeManager extends PersonManager {
+
+	
+	  public void bestEmployee() {
+		  System.out.println("Ayin Elemani Getirildi");
+	  }
+	  
+}
+	  
+	  
+	  
+	  
+	  
+	  
+
---
+++ OdevGithub/Ders24_Inheritance/src/PersonManager.java
@@ -0,0 +1,11 @@
+
+public class PersonManager {
+	public void List() {
+		  System.out.println("Musteri Listelendi");
+		  
+	  }
+	  public void Add() {
+		  System.out.println("Musteri Eklendi");
+		  
+	  }
+}
